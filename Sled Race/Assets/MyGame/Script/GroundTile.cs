0e8ea8e5275780f71e15eac273cb9cf9e48f6760
Narsiles <92451486+Narsiles@users.noreply.github.com>
2022-11-23 17:57:20 +0100

    Bientot fini

---
+++ Sled Race/Assets/MyGame/Script/GroundTile.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    Ground groundSpawner;
+    GameObject playerRef;
+    private int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundSpawner = GameObject.FindObjectOfType<Ground>();
+        SpawnObstacle();
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {      
+            //groundSpawner.SpawnTile();
+            Destroy(gameObject, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Physics.gravity);
+    }
+
+    public GameObject obstaclePrefab;
+
+    void SpawnObstacle ()
+    {
+        int obstacleSpawnIndex = Random.Range(2, transform.childCount);
+        Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+        if (spawnPoint.childCount > 0)
+        {
+            for(int i = 0; i < spawnPoint.childCount; i++)
+            {
+                Instantiate(obstaclePrefab, spawnPoint.GetChild(i).position, Quaternion.identity, transform);
+            }      
+        }
+        else
+        {
+            Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
+        }
+        
+
+    }
+
+}
---
+++ Sled Race/Assets/MyGame/Script/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 moveVector;
+    bool alive = true;
+    public Rigidbody rb;
+    [SerializeField] float vitesseDeplacement = 5f;
+    [SerializeField] float minVelocityZ = 5;
+    [SerializeField] GameObject ground;
+    [SerializeField] float velocityMax = 20;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        //print("en vie : " + alive+" "+ rb.velocity);
+
+        if (!alive) return;
+    }
+    private void FixedUpdate()
+    {
+        if (rb.velocity.z < minVelocityZ)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, minVelocityZ);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rb.velocity += new Vector3(-vitesseDeplacement, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity += new Vector3(vitesseDeplacement, 0, 0);
+        }
+
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetMouseButton(0))
+        {
+            if (Input.mousePosition.x > Screen.width / 2)
+            {
+                rb.velocity += new Vector3(vitesseDeplacement, 0, 0);
+                
+
+            }
+            else
+            {
+                rb.velocity += new Vector3(-vitesseDeplacement, 0, 0);
+            }
+        }
+
+        if (rb.velocity.magnitude > velocityMax)
+        {
+            rb.velocity = rb.velocity.normalized * velocityMax;
+        }
+    }
+    public void Die ()
+    {
+        alive = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
